276df3d92d2e24209c08bcf9254a7265d88402f2
Ian <69704125+ian-cunha@users.noreply.github.com>
2024-01-17 17:12:26 -0300

    update data

--- src/components/structure.jsx
+++ src/components/structure.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { auth } from "../config/firebase"
+import { auth, storeApp } from "../config/firebase"
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react"
 
 export const Structure = () => {
 
@@ -7,6 +10,26 @@ export const Structure = () => {
   const email = user.email;
   const name = user.displayName;
   const photo = user.photoURL;
+  const uid = user.uid;
+
+  const [dataBase, setDataBase] = useState('')
+
+  const getDataBase = async () => {
+    const docRef = doc(storeApp, "profiles", uid)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+      // console.log("Document data:", docSnap.data())
+      setDataBase(docSnap.data())
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!")
+    }
+  }
+
+  useEffect(() => {
+    getDataBase()
+  }, [])
 
   return (
     <div className="structure">
@@ -21,22 +44,21 @@ export const Structure = () => {
             <p className="email-top">{email}</p>
           </div>
         </div>
-        <p>Em breve você poderá ter seu currículo atualizado e de fácil acesso.</p>
 
-        <div>
-          <div>
+        <div className="data">
+          <div className="resume">
             <h3>Resumo</h3>
-            <p>...</p>
+            <p>{dataBase.resume}</p>
           </div>
 
           <div>
             <h3>Experiência</h3>
-            <p>...</p>
+            <p>{dataBase.experience}</p>
           </div>
 
           <div>
             <h3>Formação</h3>
-            <p>...</p>
+            <p>{dataBase.formation}</p>
           </div>
         </div>
 
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,16 +1,24 @@
-import { auth } from "../config/firebase"
+import { auth, storeApp } from "../config/firebase"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { updateProfile } from "firebase/auth";
 import { NavBar } from "../components/navbar";
 
+import { doc, setDoc } from "firebase/firestore";
+
 export const Profile = () => {
 
   const [displayName, setDisplayName] = useState('')
   const [photoURL, setPhotoURL] = useState('')
+  const [resume, setResume] = useState('')
+  const [experience, setExperience] = useState('')
+  const [formation, setFormation] = useState('')
 
   const handleDisplayName = (event) => setDisplayName(event.target.value)
   const handlePhotoURL = (event) => setPhotoURL(event.target.value)
+  const handleResume = (event) => setResume(event.target.value)
+  const handleExperience = (event) => setExperience(event.target.value)
+  const handleFormation = (event) => setFormation(event.target.value)
 
   const handleUpdate = () => {
     updateProfile(auth.currentUser, {
@@ -27,6 +35,20 @@ export const Profile = () => {
   const email = user.email;
   const name = user.displayName;
   const photo = user.photoURL;
+  const uid = user.uid;
+
+  const newDoc = async (event) => {
+    event.preventDefault()
+
+    await setDoc(doc(storeApp, "profiles", uid), {
+      resume: resume,
+      experience: experience,
+      formation: formation,
+    });
+
+    alert('Atualizado')
+
+  }
 
   return (
     <section>
@@ -41,18 +63,40 @@ export const Profile = () => {
           <p>Email: {email}</p>
         </div>
 
-        <form>
-          <h2>Dados do perfil</h2>
-          <div>
-            <label className="labelHome" htmlFor="name">Nome</label><br />
-            <input className="inputHome" type="name" id="name" placeholder="Nome do usuário (obrigatório)" onChange={handleDisplayName} />
-          </div>
-          <div>
-            <label className="labelHome" htmlFor="photo">Foto</label><br />
-            <input className="inputHome" type="text" id="photo" placeholder="URL da foto de perfil" onChange={handlePhotoURL} />
-          </div>
-        </form>
-        <button className="btn-nav" onClick={handleUpdate}>Confirmar</button>
+        <h2>Dados do perfil</h2>
+
+        <div className="updateProfile">
+          <form className="formTop">
+            <h3>Perfil</h3>
+            <div>
+              <label className="labelHome" htmlFor="name">Nome</label><br />
+              <input className="inputHome" type="name" id="name" placeholder="Nome do usuário (obrigatório)" onChange={handleDisplayName} />
+            </div>
+            <div>
+              <label className="labelHome" htmlFor="photo">Foto</label><br />
+              <input className="inputHome" type="text" id="photo" placeholder="URL da foto de perfil" onChange={handlePhotoURL} />
+            </div>
+            <button className="btn-nav" type="submit" onClick={handleUpdate}>Aplicar</button>
+          </form>
+
+          <form className="formData">
+            <h3>Dados</h3>
+            <div>
+              <label className="labelHome" htmlFor="title">Resumo</label><br />
+              <input className="inputHome" type="text" id="title" placeholder="Resumo" onChange={handleResume} />
+            </div>
+            <div>
+              <label className="labelHome" htmlFor="experience">Experiência</label><br />
+              <input className="inputHome" type="text" id="experience" placeholder="Resumo" onChange={handleExperience} />
+            </div>
+            <div>
+              <label className="labelHome" htmlFor="formation">Formação</label><br />
+              <input className="inputHome" type="text" id="formation" placeholder="Resumo" onChange={handleFormation} />
+            </div>
+            <button className="btn-nav" type="submit" onClick={newDoc}>Confirmar</button>
+          </form>
+        </div>
+
         <button className="btn-nav" onClick={() => navigate('/private')}>Voltar</button>
       </div>
     </section>
